03f6d9995508b6760bc1ff556f8e4311b363b9ea
Tim Sonnier <tisonnier2000@gmail.com>
2016-05-30 14:39:44 -0500

    Frustration

---
+++ Simple Timer/MainWindowVM.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Simple_Timer
+{
+    public class MainWindowVM : INotifyPropertyChanged
+    {
+        private TimerModel tMod = new TimerModel();
+
+        private bool _running = false;
+        private System.Threading.Thread _timerThread;
+
+        public MainWindowVM()
+        {
+            _timerThread =  new System.Threading.Thread(tMod.Run);
+            _timerThread.Start();
+        }
+
+        public string Seconds
+        {
+            get { return tMod.Seconds.ToString(); }
+            set { tMod.Seconds = Convert.ToDouble(value);
+                RaisePropertyChanged("Seconds");
+            }
+        }
+
+        public string Minutes
+        {
+            get { return tMod.Minutes.ToString(); }
+            set { tMod.Minutes = Convert.ToInt64(value);
+                RaisePropertyChanged("Minutes");
+            }
+        }
+
+        #region ICommands
+
+        void ToggleRunningExecute()
+        {
+            tMod.ToggleRunning();
+        }
+  
+        bool CanToggleRunningExecute()
+        {
+            return true;
+        }
+  
+        public ICommand ToggleRunning
+        {
+            get { return new RelayCommand(ToggleRunningExecute, CanToggleRunningExecute); }
+        }
+
+        void ResetExecute()
+        {
+            tMod.Reset();
+            RaisePropertyChanged("Seconds");
+            RaisePropertyChanged("Minutes");
+        }
+
+        bool CanResetExecute()
+        {
+            return true;
+        }
+
+        public ICommand Reset
+        {
+            get { return new RelayCommand(ResetExecute, CanResetExecute); }
+        }
+        #endregion
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Methods
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            // take a copy to prevent thread issues
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+
+    }
+}
--- Simple Timer/TimerModel.cs
+++ Simple Timer/TimerModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Simple_Timer
 {
@@ -11,7 +11,50 @@ namespace Simple_Timer
         public double Seconds { get; internal set; }
         public long Minutes { get; internal set; }
 
-        private double _seconds;
-        private long _minutes;
+        private bool _running = false;
+        private DateTime _previousTime, _currentTime;
+
+        public TimerModel()
+        {
+            _running = false;
+            Seconds = 0.0;
+            Minutes = 0;
+            _previousTime = DateTime.Now;
+            _currentTime = DateTime.Now;
+        }
+
+        public void ToggleRunning()
+        {
+            _running = !_running;
+            if(_running)
+            {
+                _previousTime = DateTime.Now;
+                _currentTime = DateTime.Now;
+            }
+        }
+
+        public void Reset()
+        {
+            _running = false;
+            Seconds = 0.0;
+            Minutes = 0;
+        }
+
+        public void Run()
+        {
+            if(_running)
+            {
+                TimeSpan delta;
+                _currentTime = DateTime.Now;
+                delta = (_currentTime - _previousTime);
+                Seconds += delta.TotalSeconds;
+                if(Seconds >= 60.0)
+                {
+                    Seconds -= 60.0;
+                    Minutes++;
+                }
+            }
+            Thread.Sleep(1);
+        }
     }
 }
