d0112f86ec33083fdbb280c72b89572b587e29e3
Tim Sonnier <tisonnier2000@gmail.com>
2016-05-29 22:23:59 -0500

    Initial commit

---
+++ Simple Timer/MainWindowMV.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Timer
+{
+    class MainWindowMV
+    {
+        private TimerModel tMod = new TimerModel();
+
+        public string Seconds
+        {
+            get { return tMod.Seconds.ToString(); }
+            set { tMod.Seconds = Convert.ToDouble(value); }
+        }
+
+        public string Minutes
+        {
+            get { return tMod.Minutes.ToString(); }
+            set { tMod.Minutes = Convert.ToInt64(value); }
+        }
+
+
+
+    }
+}
---
+++ Simple Timer/TimerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Timer
+{
+    class TimerModel
+    {
+        public double Seconds { get; internal set; }
+        public long Minutes { get; internal set; }
+
+        private double _seconds;
+        private long _minutes;
+    }
+}
