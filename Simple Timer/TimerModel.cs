3d6c42b02c9dd2c2d0be6fc5ec5ab51e0330e7c3
Tim Sonnier <tisonnier2000@gmail.com>
2016-05-30 14:06:41 -0500

    Need to work out some bugs

--- Simple Timer/TimerModel.cs
+++ Simple Timer/TimerModel.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace Simple_Timer
 {
@@ -11,7 +13,55 @@ namespace Simple_Timer
         public double Seconds { get; internal set; }
         public long Minutes { get; internal set; }
 
-        private double _seconds;
-        private long _minutes;
+        private bool _running = false;
+
+        private DateTime _last, _current;
+
+        public TimerModel()
+        {
+            Seconds = 0.0;
+            Minutes = 0;
+            _last = DateTime.Now;
+            _current = DateTime.Now;
+        }
+
+        public void ToggleRunning()
+        {
+            _running = !_running;
+            if(_running)
+            {
+                _last = DateTime.Now;
+                _current = DateTime.Now;
+            }
+            return;
+        }
+
+        public void Reset()
+        {
+            _running = false;
+            Seconds = 0.0;
+            Minutes = 0;
+        }
+
+        public void Run()
+        {
+            if (!_running)
+                Thread.Sleep(1);
+            else
+            {
+                double delta;
+                _current = DateTime.Now;
+                delta = Convert.ToDouble(_current - _last);
+                Seconds += delta;
+                if(Seconds >= 60.0)
+                {
+                    Seconds -= 60.0;
+                    Minutes++;
+                }
+
+                Thread.Sleep(1);
+                
+            }
+        }
     }
 }
